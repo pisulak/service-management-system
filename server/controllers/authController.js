506cea8ca2430b73153e9783fa3675e4958aa9fb
Kacper Pisula <127031073+pisulak@users.noreply.github.com>
2025-05-25 11:10:56 +0200

    Added password hashing and salt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const pool = require("../db");
+const { hashPassword, verifyPassword } = require("../utils/passwordUtils.js");
 
 exports.register = async (req, res) => {
   const { email, password } = req.body;
@@ -11,10 +12,13 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: "Użytkownik już istnieje" });
     }
 
+    const hashedPassword = await hashPassword(password);
+
     await pool.query("INSERT INTO users (email, password) VALUES ($1, $2)", [
       email,
-      password,
+      hashedPassword,
     ]);
+
     res.status(201).json({ message: "Rejestracja zakończona sukcesem" });
   } catch (err) {
     console.error("Rejestracja błąd:", err);
@@ -26,20 +30,28 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const result = await pool.query(
-      "SELECT * FROM users WHERE email = $1 AND password = $2",
-      [email, password]
-    );
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
-    if (result.rows.length > 0) {
-      req.session.user = {
-        id: result.rows[0].id,
-        email: result.rows[0].email,
-      };
-      res.json({ message: "Zalogowano pomyślnie" });
-    } else {
-      res.status(401).json({ message: "Niepoprawne dane logowania" });
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ message: "Niepoprawne dane logowania" });
     }
+
+    const isValid = await verifyPassword(user.password, password);
+
+    if (!isValid) {
+      return res.status(401).json({ message: "Niepoprawne dane logowania" });
+    }
+
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+    };
+
+    res.json({ message: "Zalogowano pomyślnie" });
   } catch (err) {
     console.error("Logowanie błąd:", err);
     res.status(500).json({ message: "Błąd serwera" });
